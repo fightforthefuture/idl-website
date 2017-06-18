98cafef0821b35e3f061d5eea168e7825e86c449
Jeff Lyon <jeff@rubbingalcoholic.com>
2017-06-18 20:47:45 +0000

    changes to idl website

--- public/js/internetdefenseleague.js
+++ public/js/internetdefenseleague.js
@@ -1,3 +1,48 @@
+function twitterModal() {
+    var url = 'https://mothership-js.fightforthefuture.org/connect/twitter?tag=idl';
+    var properties = 'width=600,height=500,toolbar=no,status=no,menubar=no';
+
+    window.open(url, 'idl_connect', properties);
+}
+
+(function (doc, win) {
+    "use strict";
+
+    function triggerComponents() {
+        win.components = win.components || {};
+        var
+            i = 0,
+            components = doc.getElementsByTagName('body')[0].dataset.components;
+
+        if (components !== undefined) {
+            components = components.split(' ');
+            i = components.length;
+
+            while (i--) {
+                if (components[i] !== '' && win.components[components[i]] !== undefined) {
+                    win.components[components[i]](doc, win);
+                }
+            }
+        }
+    }
+
+    triggerComponents();
+
+    var volunteer = doc.querySelector('input[name="volunteer"]');
+    if (volunteer) {
+        volunteer.addEventListener('click', function(e) {
+            var commentBox = doc.querySelector("section.comment");
+            if (e.target.checked) {
+                commentBox.classList.remove('hidden');
+            } else {
+                commentBox.classList.add('hidden');
+            }
+        });
+    }
+
+})(document, window);
+
+/*
 $(function() {
     var $form = $('form.action_form'),
         $button = $form.find('#gotime');
@@ -23,3 +68,5 @@ $(function() {
         e.preventDefault();
     })
 });
+
+*/
---
+++ public/js/petitions.js
@@ -0,0 +1,88 @@
+window.components = window.components || {};
+window.components.petitions = function (doc, win) {
+  /**
+   * Retrieves petition data from Action Network API, then submits signature
+   * @param {object} doc - Document object
+   * @param {object} win - Window object
+   * */
+  "use strict";
+
+  var
+    body = doc.getElementsByTagName('body')[0],
+    petitionSignatureForm = doc.getElementById('petition-form'),
+    apiHost = petitionSignatureForm.dataset.host,
+    objectIdentifier = petitionSignatureForm.dataset.petitionId,
+    submitButton = petitionSignatureForm.querySelector('[type="submit"]');
+
+
+  function submitForm(event) {
+    /**
+     * Submits the form to ActionNetwork. If the script doesn’t, by now, know
+     * the action_network identifier, default isn’t prevented on the event and
+     * form submission proceeds as normal.
+     * @param {event} event - Form submission event
+     * */
+    if (objectIdentifier) {
+      event.preventDefault();
+    }
+
+    var
+      signatureSubmission = new XMLHttpRequest();
+
+    // win.callbacks.petitions.preSubmit();
+
+    function compilePayload() {
+      /**
+       * Compiles the form data into a JSON payload for Ajax submission
+       * @return {object} petitionFormData - just the info the API needs
+       * */
+      var tags = ['idl'];
+
+      var formData = new FormData();
+      formData.append('guard', '');
+      formData.append('hp_enabled', true);
+      formData.append('org', 'fftf');
+      formData.append('an_tags', JSON.stringify(tags));
+      formData.append('an_url', win.location.href);
+      formData.append('contact_congress', 0);
+      formData.append('an_petition', petitionSignatureForm.action.replace('/signatures', ''));
+      formData.append('member[first_name]', doc.getElementById('form-first_name').value);
+      formData.append('member[email]', doc.getElementById('form-email').value);
+      formData.append('member[postcode]', doc.getElementById('form-zip_code').value);
+      formData.append('member[country]', "US");
+
+      if (doc.getElementById('form-comments') && doc.getElementById('form-comments').value) {
+        formData.append('action_comment', doc.getElementById('form-comments').value);
+      }
+
+      formData.append('tag', "idl");
+      
+
+      return formData;
+    }
+
+    signatureSubmission.open('POST', 'https://queue.fightforthefuture.org/action', true);
+    // signatureSubmission.open('POST', 'http://localhost:9001/action', true); // JL DEBUG ~
+    signatureSubmission.addEventListener('error', function() {
+      alert('An error occurred while signing up :(');
+    });
+    signatureSubmission.addEventListener('load', function() {
+      window.location.href = '/confirm';
+      // alert('yay');
+    });
+    signatureSubmission.send(compilePayload());
+  }
+
+  function addEventListeners() {
+    /**
+     * Attaches all the listeners all the places
+     * */
+    petitionSignatureForm.addEventListener('submit', submitForm);
+  }
+
+  function init() {
+    addEventListeners();
+  }
+
+  init();
+};
